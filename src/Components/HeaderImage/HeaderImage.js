ed2a5c9fa9a718dc48b57c4fb184a3c09c2384d7
mgru <mgru@bernafon.com>
2018-10-16 15:27:12 +0200

    refactoring

--- src/Components/HeaderImage/HeaderImage.js
+++ src/Components/HeaderImage/HeaderImage.js
@@ -3,7 +3,7 @@ import styles from './HeaderImage.module.css';
 
 export const HeaderImage = (props) => (
   <Fragment>
-      <div className={styles.headerImage}></div>
+      <div className={styles.headerBackground}></div>
       <h1 className={styles.imgTitle}>{props.title}</h1>
   </Fragment>
 );
